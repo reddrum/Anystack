b5f422875111b3eddce7b7ad27f6775eac36f2de
“reddrum” <anton.kranz@me.com>
2016-10-17 23:19:14 +0300

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,25 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: 'User was successfully created.'
+    else
+      render action: 'new', alert: 'User could not be created'
+    end
+
+  end
+
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name, :description, :skills, :files, :avatar)
+  end
+
 end
